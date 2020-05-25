689d0028480f114a8cd2d0426fa5b8010dcb6791
ichelite <edison.hua@gmail.com>
2020-05-25 18:16:21 -0400

    Updates

---
+++ precache-manifest.7cb7f05451da6eec7d83cbc2e9532665.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "063881d5ee7a32cd5ef37a3637fcaee2",
+    "url": "/react-grata/index.html"
+  },
+  {
+    "revision": "c1b00ad6feaf5516c9da",
+    "url": "/react-grata/static/css/main.571bd573.chunk.css"
+  },
+  {
+    "revision": "6b5157d0bebd1a2cd9c6",
+    "url": "/react-grata/static/js/2.8e51ccd5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-grata/static/js/2.8e51ccd5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c1b00ad6feaf5516c9da",
+    "url": "/react-grata/static/js/main.d7aa1caf.chunk.js"
+  },
+  {
+    "revision": "9a47b4cc2aa84c966997",
+    "url": "/react-grata/static/js/runtime-main.c2f0a200.js"
+  }
+]);
